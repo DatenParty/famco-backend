2489894e2a620bcafa01e86c026daf66c4ab5eac
Maschini <maschinigames@gmail.com>
2017-12-27 18:18:09 +0100

    #14 fixed maven and deleted an empty line in utlities.java

--- src/main/java/Utilities.java
+++ src/main/java/Utilities.java
@@ -4,7 +4,6 @@ import java.time.format.DateTimeFormatter;
 public class Utilities {
 	
 	static void log(String text) {
-		
 		System.out.println(LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm")) + " > " + text);
 	}
 }
